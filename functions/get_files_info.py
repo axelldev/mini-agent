8023b539409e797f7d23f28f52a41f4006552bc0
axelldev <axellsolisrubio@gmail.com>
2026-02-25 14:55:19 -0600

    feat: add get_files_info tool

---
+++ functions/get_files_info.py
@@ -0,0 +1,29 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_directory_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_directory_abs, directory))
+        valid_target_dir = (
+            os.path.commonpath([working_directory_abs, target_dir])
+            == working_directory_abs
+        )
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory} as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        contents = os.listdir(target_dir)
+
+        output = ""
+        for content in contents:
+            content_path = os.path.join(target_dir, content)
+            is_dir = os.path.isdir(content_path)
+            size = os.path.getsize(content_path)
+            output += f"- {content}: file_size={size} bytes, is_dir={is_dir}\n"
+
+        return output
+    except Exception as e:
+        return f"Error: {e}"
--- main.py
+++ main.py
@@ -1,32 +1,39 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import argparse
 
 
-load_dotenv()
+def main():
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-api_key =  os.getenv("GEMINI_API_KEY")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="user prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-client = genai.Client(api_key=api_key)
+    try:
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+        ]
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", contents=messages
+        )
+        if response is None or response.usage_metadata is None:
+            raise Exception("Usage metadata not found")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="user prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
+        print("Response:", response.text)
+    except Exception as e:
+        print("Error", e)
 
 
-try:
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
-    if not response.usage_metadata:
-        raise Exception("Usage metadata not found")
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-        
-    print("Response:", response.text)
-except Exception as e:
-    print("Error", e)
+if __name__ == "__main__":
+    main()
